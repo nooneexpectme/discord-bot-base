0771015aa472327202820421d2ca3dec68a1dc79
nooneexpectme <erwann0danguy@gmail.com>
2018-05-25 16:24:58 +0200

    Update TSLint rules

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,32 +1,19 @@
 interface QueenDecimSettings {
-    token: string,
-    prefix: string,
-    ownerId?: string,
+    token: string
+    prefix: string
+    ownerId?: string
     throwErrorPM?: boolean
 }
 
-interface CommandRequest {
-    command: CommandEntry,
-    parameters: {[name: string]: string},
-    isValid: boolean
-}
-
-interface CommandEntry {
-    name: string,
-    description: string,
-    instance: any
-}
-
 interface CommandSettings {
-    name: string,
-    description: string,
-    parameter?: CommandSettingsParameter,
-    parameters?: CommandSettingsParameter[],
+    name: string
+    description: string
+    parameters?: CommandSettingsParameter[]
     aliases?: string[]
 }
 
 interface CommandSettingsParameter {
-    name: string,
-    value: RegExp,
+    name: string
+    regEx: RegExp
     type?: any
 }
