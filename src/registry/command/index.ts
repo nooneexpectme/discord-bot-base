ec7fcbe248505a9c4a37eea22dff9a0c1a0a9d3a
nooneexpectme <erwann0danguy@gmail.com>
2018-06-06 19:48:34 +0200

    Transform methods to promises, add load/unload commands

--- src/registry/command/index.ts
+++ src/registry/command/index.ts
@@ -4,9 +4,17 @@ import { CommandBase } from '../../model/CommandBase'
 import * as Debug from 'debug'
 const log = Debug('qd:registry:command')
 
+// Default commands
+const DefaultCommands = [
+    '../../command/default/help',
+    '../../command/default/eval',
+    '../../command/default/reload'
+]
+
 // Class
 export class RegistryCommand {
     private list: {[commandName: string]: CommandBase} = {}
+    private pathsToCommand: {[path: string]: string} = {}
     private client: Client
 
     public constructor(client: Client) {
@@ -18,29 +26,70 @@ export class RegistryCommand {
     }
 
     public get(name: string): CommandBase {
-        return this.exists(name) ? this.list[name] : null
+        return this.list[name] || null
     }
 
     public getNames(): string[] {
         return Object.keys(this.list)
     }
 
-    public register(path: string): RegistryCommand {
+    public getNameFromPath(path: string): string {
+        return this.pathsToCommand[path] || null
+    }
+
+    public getPathFromName(name: string): string {
+        for (const path in this.pathsToCommand)
+            if (this.pathsToCommand[path].toLowerCase().trim() === name.toLowerCase().trim())
+                return path
+        return null
+    }
+
+    public getPaths(): string[] {
+        return Object.keys(this.pathsToCommand)
+    }
+
+    public async register(path: string): Promise<void> {
         const command: CommandBase = new (require(path))(this.client)
 
         if (this.list[command.settings.name] !== undefined) {
             log('The command is already registered (%o).', { name: command.settings.name })
-            return this
+            return
         }
 
         this.list[command.settings.name] = command
-        log('Command registered (%o).', { group: command.settings.group || null, name: command.settings.name })
-        return this
+        this.pathsToCommand[path] = command.settings.name
+        log('Command registered (%o).', {
+            group: command.settings.group || null,
+            name: command.settings.name
+        })
+        return command.load()
     }
 
-    public registerDefaults(): RegistryCommand {
-        return this
-            .register(require.resolve('../../command/default/help'))
-            .register(require.resolve('../../command/default/eval'))
+    public async unregister(path: string): Promise<void> {
+        if (this.pathsToCommand[path] === undefined) {
+            log('The command doesn\'t exists (%o).', { path })
+            return
+        }
+
+        const commandName = this.pathsToCommand[path]
+        const command = this.list[commandName]
+        const unload = command.unload()
+
+        log('Command unregistered (%o).', {
+            group: command.settings.group || null,
+            name: command.settings.name
+        })
+
+        delete this.list[commandName]
+        delete this.pathsToCommand[path]
+        return unload
+    }
+
+    public async registerDefaults(): Promise<void> {
+        await DefaultCommands.map(path => this.register(require.resolve(path)))
+    }
+
+    public async unregisterDefaults(): Promise<void> {
+        await DefaultCommands.map(path => this.unregister(require.resolve(path)))
     }
 }
