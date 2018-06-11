fa31317b50c88cc832d765936dfe8cb54b886a71
nooneexpectme <erwann0danguy@gmail.com>
2018-06-11 14:43:46 +0200

    Imporove default eval command

--- src/command/default/eval.ts
+++ src/command/default/eval.ts
@@ -2,6 +2,8 @@
 import { CommandBase } from '../../model/CommandBase'
 import { Client } from '../../'
 import { Message } from 'discord.js'
+import * as Debug from 'debug'
+const log = Debug('qd:command:eval')
 
 // Command
 module.exports = class EvalCommand extends CommandBase {
@@ -16,8 +18,10 @@ module.exports = class EvalCommand extends CommandBase {
 
     public async run(msg: Message, { requestContent }): Promise<void> {
         /* tslint:disable:no-eval */
-        const execution = eval(requestContent)
-        if (execution) await msg.channel.send(execution)
+        let execution: any = eval(requestContent)
+        if (execution instanceof Promise) execution = await execution
+        if (typeof execution !== 'object') await msg.channel.send(execution)
+        else if (execution) log('%O', execution)
         /* tslint:enable:no-eval */
     }
 }
